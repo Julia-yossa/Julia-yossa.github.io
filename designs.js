4916b2f024d36beac01a305481dac53703b017b0
Julia-yossa <40579060+Julia-yossa@users.noreply.github.com>
2018-06-27 18:15:39 +0100

    update designs.js
    i added table and the functions

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
+var color, height, width;
 // Select color input
+color = $("#colorPicker").val();
 // Select size input
-
+height = $("#inputHeight").val();
+width = $("#inputWeight").val();
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
+$("#sizePicker").submit(function makeGrid(height,width) {
 
 // Your code goes here!
-
-}
+  for( var i = 1; i <= height; i++){
+    $("table").append("<tr></tr>");
+    for ( var j = 1; j <= width; j++){
+      $("tr").append("<td></td>");
+      }
+     }
+   $("td").click(function colorChange(color){
+        $(this).css("background-color", color);
+      });
+});
